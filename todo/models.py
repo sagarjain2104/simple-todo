18b24dd443bf0e2cb3d1dd44512adb3cc0bdd4c4
sagarjain2104 <jainsagar619@gmail.com>
2024-12-18 15:57:13 +0530

    Created Task model and registered in admin

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+class Task(TimeStampedModel):
+    # Priority Choices
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+
+    PRIORITY_CHOICES = [
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (HIGH, 'High'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=LOW)
+    due_date = models.DateField(blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
